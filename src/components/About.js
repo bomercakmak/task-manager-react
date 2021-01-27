ab72cc83243f3000cb05450dd072251ed8e93971
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-01-27 18:51:03 +0100

    Added JSON Server, HTTP Requests, About Component, Routing, Footer

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom'
+
+const About = () => {
+    return (
+        <div>
+            <h3>Thanks for using my react application :)</h3>
+            <h4>Version 1.0.1</h4>
+            <Link to='/'>Go Back</Link>
+        </div>
+    )
+}
+
+export default About
